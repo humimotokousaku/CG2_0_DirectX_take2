4229a9e2bb8db2083c58746cd46a537cece5fb81
pepe <kousaku5963@gmail.com>
2023-07-13 20:46:03 +0900

    Half Lambert完了

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -23,7 +23,9 @@ PixelShaderOutput main(VertexShaderOutput input) {
 	float32_t4 textureColor = gTexture.Sample(gSampler, input.texcoord);
 	output.color = gMaterial.color * textureColor;
 	if (gMaterial.enableLighting != 0) {
-		float cos = saturate(dot(normalize(input.normal), -gDirectionalLight.direction));
+		float NdotL = saturate(dot(normalize(input.normal), -gDirectionalLight.direction));
+
+		float cos = pow(NdotL * 0.5f + 0.5f, 2.0f);
 		output.color = gMaterial.color * textureColor * gDirectionalLight.color * cos * gDirectionalLight.intensity;
 	}
 	else {
